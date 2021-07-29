5bee9d1fc303ee5571591957119de28682c3c0db
lkswally <67305217+lkswally@users.noreply.github.com>
2021-07-29 02:01:48 -0300

    Add files via upload

---
+++ nosotros.php
@@ -0,0 +1,13 @@
+<?php include("template/cabecera.php"); ?>
+
+
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Grupo de personas dedicado al desarrollo web</p>
+    <hr class="my-2">
+</div>
+
+
+
+
+<?php include("template/footer.php"); ?>
---
+++ productos.php
@@ -0,0 +1,66 @@
+<?php include("template/cabecera.php"); ?>
+
+<!-------------------------------------------------->
+<div class="col-md-3">
+
+<div class="card">
+
+    <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Libro de PHP</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+</div>
+ 
+</div>
+<!-------------------------------------------------->
+<div class="col-md-3">
+
+<div class="card">
+
+    <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Libro de PHP</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+</div>
+ 
+</div>
+<!-------------------------------------------------->
+<div class="col-md-3">
+
+<div class="card">
+
+    <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Libro de PHP</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+</div>
+ 
+</div>
+<!-------------------------------------------------->
+<div class="col-md-3">
+
+<div class="card">
+
+    <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Libro de PHP</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+</div>
+ 
+</div>
+<!-------------------------------------------------->
+
+
+
+
+
+
+<?php include("template/footer.php"); ?>
